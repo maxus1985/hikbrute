9d135fb02359db2bd62cd889451eff1a9068378f
hikbrute <nospamneededhere@gmail.com>
2024-09-13 11:11:09 +0300

    v2

--- main/Hik_v2.py
+++ main/Hik_v2.py
@@ -6,7 +6,7 @@ from main.Hik import Hik
 
 
 class HikV2(Hik):
-    auth_link = "http://{ip}:{port}/ISAPI/Security/sessionLogin/capabilities?username={username}"
+    auth_link = "http://{username}:{password}@{ip}:{port}/ISAPI/Security/sessionLogin/capabilities?username={username}"
     auth_payload = """<?xml version="1.0" encoding="utf-8"?>"""
     contentType = "application/xml"
 
@@ -15,7 +15,7 @@ class HikV2(Hik):
 
     def detect_suitability(self, ip: str, port: str) -> bool:
         """detect, if the device version is suitable for this particular module """
-        link = self.auth_link.format(ip=ip, port=port, username="dummy")
+        link = self.auth_link.format(ip=ip, port=port, username="dummy", password="dummy")
         headers = {'Content-Type': self.contentType, 'Accept':self.contentType}
 
         result = requests.post(link, headers=headers)
